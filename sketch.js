8cf7310c38bb18fc3c573aceed957b62de9ebce6
sosias <38495175+sosias@users.noreply.github.com>
2023-07-14 02:01:43 +0200

    add color palette base UI

--- sketch.js
+++ sketch.js
@@ -6,9 +6,35 @@ let bigCircleCenterY;
 let relativeSize = 1;
 let randomness = 8;
 
+let colorPalette = [
+  ["#FD2E5A","#FDE503","#1E99D2","#705791","#FE3509","#FD6E89","#7FB03B"],
+  ["#B12B2B","#DAA12F","#2D6A86","#23143D","#246B53","#D1F0E0"],
+  ["#F1EEEC","#25242A","#F2E5D3","#117989","#DBA799","#58AEBB"],
+  ["#C94A01","#C2B09D"],
+  ["#44B6C1","#E9BC44","#B8433D","#332B82"],
+  ["#694BC3","#40B7BF","#47C2A1","#CB8962"],
+  ["#822B5E","#EEE8CD","#1F1849"],
+  ["#803295","#6A9CCD","#79AAD2","#010304"],
+  ["#000000","#1C2A0E","#391D13","#391413"],
+  ["#88CDD7","#D69285","#D1A075","#2F748E","#E4EFF6"],
+  ["#A3A7E0","#608A2E","#316020","#B4C653","#C65553","#F9F7FC"],
+  ["#0B0904","#3698A1","#FAF2F0","#C4A94F","#B46A3C"],
+  ["#3872A8","#1F3E5C","#2F378E","#CB62C5","#DDECF4"],
+  ["#BB9A3E","#9D8434","#6B5A24","#DFC89F"],
+  ["#573EBB","#3C56B4","#D0BAE8","#E3ABD3"],
+  ["#2D2C4C","#0060D8","#E71192"],
+  ["#24213D","#FB0060","#06AFD6"],
+  ["#24213D","#06AFD6","#DFA900"],
+  ["#24213D","#FB0060","#06AFD6","#DFA900"],
+  ["#24213D","#06AFD6","#DFA900"],
+  ["#0F0D05","#FBEFEA","#F00F0F"],
+  ["#A87438","#B0663B", "#A46E37"]
+];
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  changeRelativeSize(1)
+  createUIPalette();
+  changeRelativeSize(1);
   runDrawFunc();
 }
 
@@ -63,4 +89,15 @@ function drawCirclesInGrid(){
 function updateInfos(){
   document.querySelector("#circlesQty").innerHTML = circleQty;
   document.querySelector("#circlesSize").innerHTML = smallCircleSize;
+}
+
+function createUIPalette(){
+  let paletteEl = document.querySelector("#palette");
+  for(let i=0; i<colorPalette.length; i++){
+    let singlePalette = "";
+    for(let j=0; j<colorPalette[i].length; j++){
+      singlePalette += '<span style="background: '+colorPalette[i][j]+'; height: 14px; width: 14px;"></span>';
+    }
+    paletteEl.innerHTML += '<div style="display: flex; height: 14px; margin-bottom:1px;">'+singlePalette+'</div>';
+  }
 }
